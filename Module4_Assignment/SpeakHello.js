445618965b3c23da6d478fedfcc22ac00aaf8b36
Kenneth Ward <59433102+K3WARD@users.noreply.github.com>
2021-02-16 19:51:07 +1300

    Add files via upload

---
+++ Module4_Assignment/SpeakHello.js
@@ -0,0 +1,16 @@
+
+(function (window) {
+var helloSpeaker = {};
+var speakWord = "Hello";
+
+ 
+ helloSpeaker.speak = function (name) {
+ var speakGreeting = speakWord + " " + name;
+ console.log(speakGreeting);
+
+}
+window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
